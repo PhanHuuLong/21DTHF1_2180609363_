fc1208b05c71afac50fd6088a76d36e514ca1d30
PhanHuuLong <long151103@gmail.com>
2023-10-09 10:30:47 +0700

    Add project files.

---
+++ Lab4_01/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab4_01.Model;
+namespace Lab4_01
+{
+    public partial class Form1 : Form
+    {
+        private List<Faculty> listFaculty = new List<Faculty>();
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StudentContextDB db = new StudentContextDB();
+
+            this.listFaculty = db.Faculties.ToList(); 
+            FillFacultyCombobox(this.listFaculty);
+            BindGrid(db.Students.ToList());
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+        private void BindGrid(List<Student> listStudent)
+        {
+            dgvStudents.Rows.Clear();
+            foreach (var student in listStudent)
+            {
+                int index = dgvStudents.Rows.Add();
+                dgvStudents.Rows[index].Cells["codeStudent"].Value = student.StudentID;
+                dgvStudents.Rows[index].Cells["name"].Value = student.FullName;
+                dgvStudents.Rows[index].Cells["averageScore"].Value = student.AverageScore;
+
+                if (student.Faculty != null && student.Faculty.FacultyName != null)
+                {
+                    dgvStudents.Rows[index].Cells["faculty"].Value = student.Faculty.FacultyName;
+                }
+            }
+        }
+        void FillFacultyCombobox (List<Faculty> listFaculty)
+        {
+            this.comboBoxFaculty.DataSource = listFaculty;
+            this.comboBoxFaculty.DisplayMember = "FacultyName";
+            this.comboBoxFaculty.ValueMember = "FacultyID";
+        }
+
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void butAdd_Click(object sender, EventArgs e)
+        {
+            StudentContextDB db = new StudentContextDB();
+
+            string studentID = txtCodeStudent.Text;
+            Student existingStudent = db.Students.FirstOrDefault(s => s.StudentID == studentID);
+
+            if (existingStudent != null)
+            {
+                MessageBox.Show("Student with the same ID already exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Student student = new Student()
+            {
+                StudentID = studentID,
+                FullName = txtName.Text,
+                FacultyID = (int)comboBoxFaculty.SelectedValue,
+            };
+
+            double averageScore;
+            if (double.TryParse(txtAverageScore.Text, out averageScore))
+            {
+                student.AverageScore = averageScore;
+
+                db.Students.Add(student);
+                db.SaveChanges();
+                BindGrid(db.Students.ToList());
+            }
+            else
+            {
+                MessageBox.Show("Invalid Average Score. Please enter a valid number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void butUpdate_Click(object sender, EventArgs e)
+        {
+            StudentContextDB db = new StudentContextDB();
+            var item = db.Students.FirstOrDefault(s => s.StudentID.Equals(txtCodeStudent.Text));
+
+            if(item==null)
+            {
+                MessageBox.Show("Found no have Code Student!");
+                return;
+            }
+            else
+            {
+                item.FullName = txtName.Text;
+                item.FacultyID = (int)comboBoxFaculty.SelectedValue;
+                item.AverageScore = double.Parse(txtAverageScore.Text);
+
+                db.SaveChanges();
+                BindGrid(db.Students.ToList());
+            }
+        }
+
+
+
+        private void butDelete_Click(object sender, EventArgs e)
+        {
+            int selectedRowIndex = dgvStudents.SelectedCells[0].RowIndex;
+            if (selectedRowIndex >= 0)
+            {
+                string studentId = dgvStudents.Rows[selectedRowIndex].Cells["codeStudent"].Value.ToString();
+
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this student?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    StudentContextDB db = new StudentContextDB();
+                    var student = db.Students.SingleOrDefault(s => s.StudentID.Equals(studentId));
+
+                    if (student != null)
+                    {
+                        db.Students.Remove(student);
+                        db.SaveChanges();
+                        BindGrid(db.Students.ToList());
+                    }
+                    else
+                    {
+                        MessageBox.Show("Student not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to delete.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void dgvStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            MessageBox.Show($"You entered at {rowIndex}.");
+
+            if (rowIndex >= 0)
+            {
+                txtCodeStudent.Text = dgvStudents.Rows[rowIndex].Cells["codeStudent"].Value.ToString();
+                txtName.Text = dgvStudents.Rows[rowIndex].Cells["name"].Value.ToString();
+                txtAverageScore.Text = dgvStudents.Rows[rowIndex].Cells["averageScore"].Value.ToString();
+
+                var facultyname = dgvStudents.Rows[rowIndex].Cells["faculty"].Value.ToString();
+                var faculty = this.listFaculty.FirstOrDefault(c => c.FacultyName.Equals(facultyname));
+
+                if (faculty == null)
+                {
+                    comboBoxFaculty.SelectedValue = null;
+                }
+                else
+                {
+                    comboBoxFaculty.SelectedValue = faculty.FacultyID;
+                }
+            }
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void manegerFacultyF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmFalculty_Click(object sender, EventArgs e)
+        {
+            FrmManagerFaculty ff = new FrmManagerFaculty();
+            ff.ShowDialog();
+        }
+
+        private void manegerFacultyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmManagerFaculty fm = new FrmManagerFaculty();
+            fm.ShowDialog();
+        }
+
+        private void findCtrlFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FindStudent fs=new FindStudent();
+            fs.ShowDialog();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.F2) {
+                FrmManagerFaculty fm = new FrmManagerFaculty();
+                fm.ShowDialog();
+            }
+            if(e.Control && e.KeyCode == Keys.F) {
+                FindStudent fs = new FindStudent();
+                fs.ShowDialog();
+            }
+        }
+
+        private void toolStripButton1_ManegerFaculty_Click(object sender, EventArgs e)
+        {
+            FrmManagerFaculty fm=new FrmManagerFaculty();
+            fm.ShowDialog();
+        }
+
+        private void toolStripLabel2_ManagerFaculty_Click(object sender, EventArgs e)
+        {
+            FrmManagerFaculty fm = new FrmManagerFaculty();
+            fm.ShowDialog();
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please enter StudentID and click one time enter.", "Confident", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void toolStripLabel_FindByName_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please enter StudentID in text.", "Confident", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Lab4_01/OrderImformation.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Lab4_01
+{
+    partial class OrderImformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvOrderInformation = new System.Windows.Forms.DataGridView();
+            this.nummers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deliveryDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.orderTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderInformation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvOrderInformation);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(752, 377);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Order Information";
+            // 
+            // dgvOrderInformation
+            // 
+            this.dgvOrderInformation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderInformation.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nummers,
+            this.orderID,
+            this.orderDate,
+            this.deliveryDate,
+            this.orderTotal});
+            this.dgvOrderInformation.Location = new System.Drawing.Point(7, 100);
+            this.dgvOrderInformation.Name = "dgvOrderInformation";
+            this.dgvOrderInformation.Size = new System.Drawing.Size(729, 271);
+            this.dgvOrderInformation.TabIndex = 6;
+            this.dgvOrderInformation.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // nummers
+            // 
+            this.nummers.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nummers.HeaderText = "Nummers";
+            this.nummers.Name = "nummers";
+            // 
+            // orderID
+            // 
+            this.orderID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.orderID.HeaderText = "OrderID";
+            this.orderID.Name = "orderID";
+            // 
+            // orderDate
+            // 
+            this.orderDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.orderDate.HeaderText = "Order Date";
+            this.orderDate.Name = "orderDate";
+            // 
+            // deliveryDate
+            // 
+            this.deliveryDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.deliveryDate.HeaderText = "Delivery Date";
+            this.deliveryDate.Name = "deliveryDate";
+            // 
+            // orderTotal
+            // 
+            this.orderTotal.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.orderTotal.HeaderText = "Order Total";
+            this.orderTotal.Name = "orderTotal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "~";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "MM/dd/yyyy";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(256, 51);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(98, 22);
+            this.dateTimePicker2.TabIndex = 4;
+            this.dateTimePicker2.Value = new System.DateTime(2019, 10, 2, 15, 46, 0, 0);
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time order";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "MM/dd/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(125, 51);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(98, 22);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Value = new System.DateTime(2019, 10, 1, 15, 46, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.dateTimePicker1.MouseLeave += new System.EventHandler(this.dateTimePicker1_MouseLeave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(38, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Show all day month";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // OrderImformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "OrderImformation";
+            this.Text = "OrderImformation";
+            this.Load += new System.EventHandler(this.OrderImformation_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderInformation)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dgvOrderInformation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nummers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deliveryDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderTotal;
+    }
+}
